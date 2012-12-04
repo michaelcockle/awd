d19ef48e888678490e5aa85c6b361fe05496e561
michaelcockle <michael@axisweb.org>
2012-12-04 15:44:57 +0000

    js change

--- js/general.js
+++ js/general.js
@@ -672,49 +672,6 @@ $(window).load(function()  {
 
 
 
-	// Hover Panels put before slide scroller
-	// set up hover panels can be done without JS, events causes hover to be triggered when element tapped on touch device
-	$('.hover').hover(function(){
-		$(this).addClass('flip');
-	},function(){
-		$(this).removeClass('flip');
-	});
-	
-	// set up click/tap panels
-	$('.click').toggle(function(){
-		$(this).addClass('flip');
-	},function(){
-		$(this).removeClass('flip');
-	});
-	
-	// set up block configuration
-	$('.block .action').click(function(){
-		$('.block').addClass('flip');
-	});
-	$('.block .edit-submit').click(function(e){
-		$('.block').removeClass('flip');
-		
-		// why not update that list for fun?
-		$('#list-title').text($('#form_title').val());
-		$('#list-items').empty();
-		for (var num = 1; num <= $('#form_items').val(); num++) {
-			$('#list-items').append('<li>Name '+num+'</li>');
-		}
-		e.preventDefault();
-	});
-	
-	// set up contact form link
-	$('.contact .action').click(function(e){
-		$('.contact').addClass('flip');
-		e.preventDefault();
-	});
-	$('.contact .edit-submit').click(function(e){
-		$('.contact').removeClass('flip');
-		// just for effect we'll update the content
-		e.preventDefault();
-	});
-		
-
 	/*	Image Slider Init --------------------------------------	 */ 
 
 	$(window).load(function(){
